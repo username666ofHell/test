6ef1f9e555550486fe8fe0bfe65d182feb7163b2
Serg <seregawz@gmail.com>
2019-04-04 03:58:58 +0300

    коммит 04.04

--- jsUdemy/main.js
+++ jsUdemy/main.js
@@ -45,7 +45,7 @@
 //     console.log(i)
 // }
 
-// ФУНКЦИИ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// ФУНКЦИИ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // let num = 20;
 
 // function showFirstMessage(text) {
@@ -63,29 +63,131 @@
 //     return (a + b);
 // }
 
-let calc = (a,b) => a+b
+// Функции !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// let calc = (a,b) => a+b
 
-console.log(calc(3,4));
+// console.log(calc(3,4));
 
-console.log(calc(8,4));
+// console.log(calc(8,4));
 
-function retVar() {
-    let num = 50;
-    return num;
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// let str = "test";
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let twelve = "12.2px";
+
+// // console.log(Math.round(twelve));
+// console.log(parseInt(twelve));
+// console.log(parseFloat(twelve));
+
+// Callback функции !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// function first(){
+//     // что-то делаем
+//     setTimeout( function(){
+//         console.log(1);
+//     }, 500 );
+// }
+
+// function second(){
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback){
+//     console.log("Я учу" + lang);
+//     callback();
+// }
+
+// function done(){
+//     console.log("Я прошел 3й урок");
+// }
+
+// learnJS("JavaScript", done);
+
+// Объекты!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// let options =  {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
+
+// console.log(options.name);
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     bg: "red"
+// };
+
+// delete options.bool;
+
+// console.log(options);
+
+// for (let key in options) {
+//     console.log('Свойство ' + key + ' имеет значение ' + options[key]);
+// }
+// console.log(Object.keys(options).length);
+
+// 013 Массивы и псевдомассивы !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// let arr = ["first", 2, 3, "fourth", 5];
+// arr[99] = 99;
+// console.log(arr.length);
+
+// arr.pop();
+// arr.push("5");
+// arr.shift();
+// arr.unshift("1");
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(item, i, mass){
+//     console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+// });
+
+// console.log(arr);
+
+
+
+// let mass = [1, 3, 4, 6, 7];
+
+// for (let key of mass) {
+//     console.log(key);
+// }
+
+
+
+// let ans = prompt("", ""),
+//     arr = [];
+
+// arr = ans.split(',');  //split помещает данные в массив
+// console.log(arr);
+
+
+
+// let arr = ["aaa", "bbb", "ccc", "ddd"],
+//     i = arr.join(', '); //join склеивает каждый элемент массива в одну строку
+
+// console.log(i);
+
+
+let arr = [1, 15, 4],
+    i = arr.sort(compareNum); //sort - сортировка
+
+function compareNum(a,b) {
+    return a-b;
 }
 
-let anotherNum = retVar();
-console.log(anotherNum);
-
-let str = "test";
-console.log(str.length);
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = "12.2px";
-
-// console.log(Math.round(twelve));
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
-
+console.log(arr);
