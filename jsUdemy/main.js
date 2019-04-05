3336e5b5d2c3d413153eeb4795e82c811ce9022d
Serg <seregawz@gmail.com>
2019-04-05 03:31:15 +0300

    Commit 05.04

--- jsUdemy/main.js
+++ jsUdemy/main.js
@@ -183,11 +183,202 @@
 // console.log(i);
 
 
-let arr = [1, 15, 4],
-    i = arr.sort(compareNum); //sort - сортировка
+// let arr = [1, 15, 4],
+//     i = arr.sort(compareNum); //sort - сортировка
 
-function compareNum(a,b) {
-    return a-b;
-}
+// function compareNum(a,b) {
+//     return a-b;
+// }
 
-console.log(arr);
+// console.log(arr);
+
+
+// ООП, прототип
+// let soldier = {
+//     health: 400,
+//     armor: 100
+// };
+
+// let john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// console.log(john);
+// console.log(john.armor);
+
+
+// 015 Отлавливаем ошибки в своем коде при помощи консоли разработчика!!!!!!
+// Breakpoints
+
+// function hello() {
+//     console.log("Hello World!");
+// }
+
+// hello();
+
+// function hi() {
+//     console.log("Hello World!");
+// }
+
+// hi();
+
+// let arr = [1, 15, 4, 30, 42],
+//     i = arr.sort(compareNum);
+
+// function compareNum(a,b) {
+//     return a-b;
+// }
+
+// console.log(arr);
+
+
+// 016 Получение элементов со страницы!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let box = document.getElementById("box"),
+//     btn = document.getElementsByTagName("button"),
+//     circle = document.getElementsByClassName("circle"),
+//     heart = document.querySelectorAll(".wrapper .heart"),
+//     oneHeart = document.querySelector(".wrapper .heart"), //первый класс heart на странице
+//     wrapper = document.querySelector(".wrapper");
+
+// console.log(box);
+// console.log(btn);
+// console.log(btn[0]);
+// console.log(circle[2]);
+// console.log(heart);
+// console.log(oneHeart);
+
+// box.style.backgroundColor = "blue";
+// btn[1].style.borderRadius = "100%";
+// circle[0].style.backgroundColor = "red";
+// circle[1].style.backgroundColor = "yellow";
+// circle[2].style.backgroundColor = "green";
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = "blue";
+// }
+
+// heart.forEach(function(item, i, hearts) {
+//     item.style.backgroundColor = "blue";
+// });
+
+
+// let div = document.createElement("div"),
+//     text = document.createTextNode("Тут был я");
+
+// div.classList.add("black");
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+
+// div.innerHTML = "<h1>Hello World!</h2>";
+// div.textContent = "Hello World!";
+
+// document.body.insertBefore(div, circle[0]);
+// document.body.removeChild(circle[1]);
+// wrapper.removeChild(heart[1]);
+
+// document.body.replaceChild(btn[1], btn[4]);
+
+// console.log(div);
+
+
+// 018 События и их обработчики!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let btn = document.querySelectorAll("button"),
+//     wrap = document.querySelector(".wrapper"),
+//     link = document.querySelector("a");
+
+// btn[0].onclick = function() {
+//     alert("Вы нажали первую кнопку!");
+// };
+
+// btn[0].onclick = function() {
+//     alert("Вы опять нажали первую кнопку!");
+// };
+
+// btn[0].addEventListener("click", function(event) {
+//     // console.log(event);
+//     // let target = event.target;
+//     // target.style.display = "none";
+//     console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+// });
+
+// wrap.addEventListener("click", function() {
+//     console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+// });
+
+// link.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+// });
+
+// btn.forEach(function(item) {
+//     item.addEventListener("mouseleave", function() {
+//         console.log("Вышли!");
+//     });
+// });
+
+// btn[0].addEventListener("click", function() {
+//     alert("Вы опять нажали первую кнопку!");    
+// });
+
+// btn[0].addEventListener("mouseenter", function() {
+//     alert("Вы навели курсор на первую кнопку");
+// });
+
+
+// 019 Скрипты и время их выполнения. setTimeout и setInterval!!!!!!!!!!!!!!!!!!
+
+// let timerId = setTimeout(sayHello, 3000);
+// clearTimeout(timerId);
+
+// let timerId = setInterval(sayHello, 3000);
+// clearTimeout(timerId);
+
+// function sayHello() {
+//     console.log("Hello World!");
+// }
+
+
+// Рекурсивный вызов setTimeout (лучше чем setInterval)
+
+// let timerId = setTimeout(function log() {
+//     console.log("Hello");
+//     setTimeout(log, 2000);
+// });
+
+// Анимация
+// let btn = document.querySelector(".btn"),
+//     elem = document.querySelector(".box");
+
+// function myAnimation() {
+//     let pos = 0;
+
+//     let id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + "px";
+//         }
+//     }
+// }
+
+// btn.addEventListener("click", myAnimation);
+
+// // Делегирование событий
+// let btnBlock = document.querySelector(".btn-block"),
+//     btns = document.getElementsByTagName("button");
+
+// btnBlock.addEventListener("click", function(event) {
+//     // if (event.target && event.target.tagName == "BUTTON") {
+//     // if (event.target && event.target.сlassList.contains("first") ) {
+//     if (event.target && event.target.matches("button.first")) {
+//         console.log("Hello");
+//     }
+// });
