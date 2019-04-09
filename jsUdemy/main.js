05931accdd81c8ada4f22711539c6ca433a277c6
Serg <seregawz@gmail.com>
2019-04-09 09:28:21 +0300

    commit 09.04

--- jsUdemy/main.js
+++ jsUdemy/main.js
@@ -526,3 +526,163 @@
 // 4) указание конкретного контекста - call, apply, bind
 
 
+// 024 Стандарт ES6. Интерполяция!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let name = "Ivan",
+//     age = 30,
+//     mail = "ex.mail.ru";
+
+// // document.write("Пользователю " + name + " " + age + " лет. Его почтовый адрес: " + mail);
+// document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
+
+
+// 025 ES6. let и const!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function makeArray() {
+//     var items = [];
+
+//     for (let i = 0; i < 10; i++) {
+//         var item = function() {
+//             console.log(i);
+//         };
+//         items.push(item);
+//     }
+
+//     return items;
+// }
+
+// var arr = makeArray();
+
+// arr[1]();
+// arr[3]();
+// arr[7]();
+
+
+
+// 026 ES6. Стрелочные функции!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 1) стрелочная ф-я анонимна - ей нельзя задать имя, только поместить в переменную;
+// нельзя управлять обработчиками событий, если это необходимо; нельзя запускать эту ф-ю
+// внутри себя
+// 2) основная особенность - стрелочная ф-я не имеет своего контекста вызова this, т.е.
+// внутри этой ф-и тот же this что и снаружи
+// чаще всего стрелочные ф-и используются в обработчиках событий, setInterval, setTimeout, ajax
+
+// let fun = () => {
+//     console.log(this);
+// };
+
+// fun();
+
+// let obj = {
+//     number: 5,
+//     sayNumber: function() {
+//         let say = () => {
+//             console.log(this);
+//         };
+//         say();
+//     }
+// };
+
+// obj.sayNumber();
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function() {
+//     let show = () => {
+//         console.log(this);
+//     };
+//     show();
+// });
+
+
+// 027 ES6. Параметры по умолчанию !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function calcOrDouble(number, basis = 2) {
+//     // basis = basis || 2; ES5
+//     console.log(number*basis);
+// }
+// calcOrDouble(3,5);
+// calcOrDouble(6);
+
+
+// 028 ES6. Классы
+
+// class Rectangle {
+//     constructor(height, width = 20) {
+//         this.height = height;
+//         this.width = width;
+//     }
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+// const square = new Rectangle(10, 10);
+
+// console.log(square.calcArea());
+
+
+// 029 ES6. Spread-операторы !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// spread разворачивает массивы и превращает их в данные
+
+// let video = ["youtube", "vimeo", "rutube"],
+//     blogs = ["wordpress", "livejournal", "blogger"],
+//     internet = [...video, ...blogs, "vk", "facebook"];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(a + b + c);
+// }
+// let numbers = [2, 5, 7];
+
+// log(...numbers);
+
+
+// 030 JSON формат передачи данных !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let options = {
+//     width: 1366,
+//     height: 768,
+//     background: "red",
+//     font: {
+//         size: "16px",
+//         color: "#fff"
+//     }
+// };
+
+// console.log(JSON.parse(JSON.stringify(options)));
+
+
+// 031 AJAX !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let inputRub = document.getElementById("rub"),
+    inputUsd = document.getElementById("usd");
+
+inputRub.addEventListener("input", () => { //стрелочная т.к. контекст вызова неважен
+    let request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass);
+    request.open("GET", "current.json");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    request.send();
+
+    //status код ответа сервера
+    //statusText (ok, not found)
+    //responceText (текст ответа сервера) / responce
+    //readyState
+
+    request.addEventListener("readystatechange", function() {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.responce);
+
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+
+});
